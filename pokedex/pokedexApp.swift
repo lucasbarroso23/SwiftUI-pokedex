1db4530fe3c7f7639efb8503d147776d2dbfef5c
barrosol <lucas.barroso@impactzero.pt>
2022-12-13 10:38:23 +0000

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Lucas Barroso IZ on 13/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
